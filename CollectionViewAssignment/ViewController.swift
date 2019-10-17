71232114cbb8209ad2b4076f68dfbc20469bec62
Ankur Baranwal <ankur.baranwal@cl-macmini-127.local>
2019-10-17 11:33:45 +0530

    Initial Commit

---
+++ CollectionViewAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewAssignment
+//
+//  Created by Ankur Baranwal on 17/10/2019.
+//  Copyright © 2019 Ankur Baranwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
